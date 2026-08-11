558404f5320aae58902cdf29a30d7f4a8d0402f9
Paul9441 <paulpk7077@gmail.com>
2026-08-11 13:10:38 +0530

    feat: add web application icons and favicon assets

--- finance_app/lib/services/auth_service.dart
+++ finance_app/lib/services/auth_service.dart
@@ -5,14 +5,14 @@ import '../models/user_model.dart';
 import 'api_service.dart';
 
 class AuthService {
-  static String baseUrl = 'http://192.168.0.13:8000/api/v1';
+  static String baseUrl = 'http://192.168.0.16:8000/api/v1';
   static String? _activeBaseUrl;
 
   static final List<String> _defaultCandidateBaseUrls = [
-    'http://127.0.0.1:8000/api/v1',
+    'http://127.0.0.16:8000/api/v1',
     'http://localhost:8000/api/v1',
     'http://10.0.2.2:8000/api/v1',
-    'http://192.168.0.13:8000/api/v1',
+    'http://192.168.0.16:8000/api/v1',
   ];
 
   static List<String> get candidateUrls {
